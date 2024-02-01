3f86d7d538fbb731553f2cb355849e1211e6d908
deepanshu <xahhishekheer08@gmail.com>
2024-02-01 12:14:37 +0530

    !done

--- lib/Ui/Auth/auth_method.dart
+++ lib/Ui/Auth/auth_method.dart
@@ -1,6 +1,5 @@
 //google_sign_in: ^4.5.1
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
